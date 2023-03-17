2a572ec48eb270dad132d4062f3d1fa2476b505c
andrew-Woo9 <woo95kil@gmail.com>
2023-03-17 18:20:16 +0900

    git remote init

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM node:alpine as builder
+
+WORKDIR '/usr/src/app'
+
+COPY package.json .
+
+RUN yarn install
+
+COPY ./ ./
+
+
+FROM nginx
+
+COPY --from=builder /usr/src/app/build /usr/share/nginx/html
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('테스트 1', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+test('테스트 2', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+test('테스트 3', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
